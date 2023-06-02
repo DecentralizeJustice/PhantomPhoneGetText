5a199e27207fae7640b64207853c27c2fd67e77e
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-01 21:59:48 -0400

    first test

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -1,16 +1,29 @@
+const axios = require("axios")
+const mongoDBPassword = process.env.mongoDBPassword
+const mongoServerLocation = process.env.mongoServerLocation
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
 exports.handler = async (event) => {
-      try {
-        console.log(event.body)
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })    
+  try {
+    console.log(event.body)
+    const collection = client.db("demo").collection("demo")
+    const docInfo = { 
+      test: true
+    }
+    const doc = docInfo
+    await collection.insertOne(doc)
+    await client.close()
+    return {
+      statusCode: 200,
+      body: ''
+    }
+    } catch (error) {
+      console.log(error)
       return {
-        statusCode: 200,
+        statusCode: 500,
         body: ''
       }
-      } catch (error) {
-        console.log(error)
-        return {
-          statusCode: 500,
-          body: ''
-        }
-      }
-  
-  }
+    }
+
+}
