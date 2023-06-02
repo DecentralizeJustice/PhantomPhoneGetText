f665c4b9d737e8beaed8be331cd9f9a891582768
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-01 22:22:17 -0400

    add content

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -3,15 +3,11 @@ const mongoDBPassword = process.env.mongoDBPassword
 const mongoServerLocation = process.env.mongoServerLocation
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
-console.log(uri)
 exports.handler = async (event) => {
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })    
   try {
-    console.log(event.body)
     const collection = client.db("demo").collection("demo")
-    const docInfo = { 
-      test: true
-    }
+    const docInfo = event.body
     const doc = docInfo
     await collection.insertOne(doc)
     await client.close()
