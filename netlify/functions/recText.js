3389ac6737b5b1764cd7674128c76e8a0cab94fc
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-01 23:11:18 -0400

    create new

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -7,13 +7,13 @@ exports.handler = async (event) => {
 try {
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
   const collection = client.db("demo").collection("demo")
-  // create array const docInfo = { messageArray: [] }
-  const docInfo = JSON.parse(event.body)
-  // await collection.insertOne(doc)
-  await collection.updateOne(
+  const docInfo = {_id: 69, messageArray: [] }
+  // const docInfo = JSON.parse(event.body)
+  await collection.insertOne(docInfo)
+/*   await collection.updateOne(
     { _id: ObjectId.toString("64795846bbff92df43d432bb") },
     { $push: { messageArray: { hi: true} } }
-  )
+  ) */
   await client.close()
   console.log('info entered')
   return {
