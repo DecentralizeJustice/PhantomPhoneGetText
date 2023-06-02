526b7bef80c7378cbbbb59794c35f31655bce771
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-01 22:42:31 -0400

    create message array

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -4,23 +4,24 @@ const mongoServerLocation = process.env.mongoServerLocation
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
 exports.handler = async (event) => {
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })    
-  try {
-    const collection = client.db("demo").collection("demo")
-    const docInfo = JSON.parse(event.body)
-    const doc = docInfo
-    await collection.insertOne(doc)
-    await client.close()
+try {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+  const collection = client.db("demo").collection("demo")
+  const docInfo = { messageArray: [] }
+  const doc = docInfo
+  await collection.insertOne(doc)
+  await client.close()
+  console.log('info entered')
+  return {
+    statusCode: 200,
+    body: ''
+  }
+  } catch (error) {
+    console.log(error)
     return {
-      statusCode: 200,
+      statusCode: 500,
       body: ''
     }
-    } catch (error) {
-      console.log(error)
-      return {
-        statusCode: 500,
-        body: ''
-      }
-    }
+  }
 
 }
