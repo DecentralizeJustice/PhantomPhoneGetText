998287aa20800f5c5151ca9f591d2eefa6508aed
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-04 16:13:53 -0400

    no password

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -5,18 +5,26 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
 exports.handler = async (event) => {
   try {
-    if (event.headers.specpassword === apiPassword) {
-      await addText(event)
-    }
-    else {
-      throw "Wrong Password"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+    const collection = client.db("demo").collection("demo")
+    // const docInfo = {_id: 69, messageArray: [] }
+    // await collection.insertOne(docInfo)
+    await collection.updateOne(
+      { _id: 69 },
+      { $push: { messageArray: JSON.parse(event.body) } }
+    )
+    await client.close()
+    console.log('info entered')
+    return {
+      statusCode: 200,
+      body: ''
     }
   } 
   catch (error) {
     console.log(error)
     return {
-      statusCode: 500,
-      body: ''
+      statusCode: 200,
+      body: 'broke'
     }
   }
 }
