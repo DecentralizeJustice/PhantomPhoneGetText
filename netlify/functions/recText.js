bb35a430371db6134fb910c29c578dad02f4e387
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-04 17:01:53 -0400

    reuse connection

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -3,18 +3,18 @@ const mongoServerLocation = process.env.mongoServerLocation
 const apiPassword = process.env.apiPassword
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+const collection = client.db("demo").collection("demo")
 exports.handler = async (event) => {
   try {
     if (event.headers.specpassword === apiPassword) {
-      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-      const collection = client.db("demo").collection("demo")
       // const docInfo = {_id: 69, messageArray: [] }
       // await collection.insertOne(docInfo)
       await collection.updateOne(
         { _id: 69 },
         { $push: { messageArray: JSON.parse(event.body) } }
       )
-      await client.close()
+      //await client.close()
       console.log('info entered')
       return {
         statusCode: 200,
