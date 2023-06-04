cb3c2e0af870073f59d526dde5f260ffa9038258
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-04 16:22:40 -0400

    addpassword

--- netlify/functions/recText.js
+++ netlify/functions/recText.js
@@ -5,26 +5,34 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
 exports.handler = async (event) => {
   try {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-    const collection = client.db("demo").collection("demo")
-    // const docInfo = {_id: 69, messageArray: [] }
-    // await collection.insertOne(docInfo)
-    await collection.updateOne(
-      { _id: 69 },
-      { $push: { messageArray: JSON.parse(event.body) } }
-    )
-    await client.close()
-    console.log('info entered')
-    return {
-      statusCode: 200,
-      body: ''
+    if (event.headers.specpassword === apiPassword) {
+      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+      const collection = client.db("demo").collection("demo")
+      // const docInfo = {_id: 69, messageArray: [] }
+      // await collection.insertOne(docInfo)
+      await collection.updateOne(
+        { _id: 69 },
+        { $push: { messageArray: JSON.parse(event.body) } }
+      )
+      await client.close()
+      console.log('info entered')
+      return {
+        statusCode: 200,
+        body: ''
+      }
+    } else {
+      console.log('wrong password')
+      return {
+        statusCode: 200,
+        body: ''
+      }
     }
   } 
   catch (error) {
     console.log(error)
     return {
       statusCode: 500,
-      body: 'broke'
+      body: ''
     }
   }
 }
