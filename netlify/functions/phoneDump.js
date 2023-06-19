102e5749fc110565d6f9b804cbc5655b1e3a52d6
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-19 15:07:09 -0700

    test push new phone

--- netlify/functions/phoneDump.js
+++ netlify/functions/phoneDump.js
@@ -4,18 +4,18 @@ const apiPassword = process.env.apiPassword
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-const collection = client.db("demo").collection("demo")
+const collection = client.db("real").collection("phonedump")
 exports.handler = async (event) => {
   try {
     if (event.headers.specpassword === apiPassword) {
-      // const docInfo = {_id: 69, messageArray: [] }
+      const phoneName = JSON.parse(event.body).phone
+      console.log(phoneName)
+      //const docInfo = {_id: 69, messageArray: [] }
       // await collection.insertOne(docInfo)
-/*       await collection.updateOne(
-        { _id: 69 },
+      await collection.updateOne(
+        { "phone" : phoneName },
         { $push: { messageArray: JSON.parse(event.body) } }
-      ) */
-      //await client.close()
-      // console.log('info entered')
+      )
       console.log(event.body)
       return {
         statusCode: 200,
