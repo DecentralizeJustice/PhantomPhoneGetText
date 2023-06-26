7cc62a10d34daedc53d3aeee7347739e439a5b3d
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-25 23:08:42 -0400

    push new function

---
+++ netlify/functions/phoneDumpPhoneLong.js
@@ -0,0 +1,37 @@
+const mongoDBPassword = process.env.mongoDBPassword
+const mongoServerLocation = process.env.mongoServerLocation
+const apiPassword = process.env.apiPassword
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+const collection = client.db("real").collection("phoneDumpPhoneLong")
+exports.handler = async (event) => {
+  try {
+    if (event.headers.specpassword === apiPassword) {
+      const phoneName = JSON.parse(event.body).phone
+      //const docInfo = {_id: 69, messageArray: [] }
+      // await collection.insertOne(docInfo)
+      await collection.updateOne(
+        { "phone" : phoneName },
+        { $push: { messageArray: JSON.parse(event.body) } }
+      )
+      return {
+        statusCode: 200,
+        body: ''
+      }
+    } else {
+      console.log('wrong password')
+      return {
+        statusCode: 200,
+        body: ''
+      }
+    }
+  } 
+  catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: ''
+    }
+  }
+}
